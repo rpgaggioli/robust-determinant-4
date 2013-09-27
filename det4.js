14ad274745e8b83fc70d22579a8f579a8079af36
rpgaggioli <rpgaggioli@wisc.edu>
2013-09-27 15:34:36 -0500

    add det4

---
+++ det4.js
@@ -0,0 +1,15 @@
+
+
+var robustSum = require("robust-sum")
+var robustScale = require("robust-scale")
+var det3 = require("robust-determinant-3")
+
+module.exports = robustDeterminant4
+
+function robustDeterminant4(m) {
+  var A = robustScale(det3([[m[1][1], m[1][2], m[1][3]], [m[2][1], m[2][2], m[2][3]], [m[3][1], m[3][2], m[3][3]]]), m[0][0])
+  var B = robustScale(det3([[m[1][0], m[1][2], m[1][3]], [m[2][0], m[2][2], m[2][3]], [m[3][0], m[3][2], m[3][3]]]), m[0][1])
+  var C = robustScale(det3([[m[1][0], m[1][1], m[1][3]], [m[2][0], m[2][1], m[2][3]], [m[3][0], m[3][1], m[3][3]]]), m[0][2])
+  var D = robustScale(det3([[m[1][0], m[1][1], m[1][2]], [m[2][0], m[2][1], m[2][2]], [m[3][0], m[3][1], m[3][2]]]), m[0][3])
+  return robustSum(robustSum(A, B), robustSum(C, D))
+}
